28ec8085f1bf74a1eb0252d4f47bb91b22769ce5
shahd-mohamed778 <shahd.hamed61002@gmail.com>
2024-11-18 01:28:45 +0200

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,12 @@
+$( function() {
+    $( "#inputcheckin" ).datepicker();
+    $( "#inputcheckout" ).datepicker();
+      
+    $('.level-slider').slick({
+    dots:true, 
+    slidesToShow :2,
+    slidesToScroll : 1,
+    arrows: false,
+    });
+
+  } );
